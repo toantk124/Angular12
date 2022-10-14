ef9af7d70f4cc6ced1b8325f4972541d58351e1f
toantk124 <khanhtoantran1101@gmail.com>
2022-10-14 12:24:02 +0700

    DB-ngIF

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+ public name ='Khánh Toàn';
+ public age = 25;
   constructor() { }
 
   ngOnInit(): void {
