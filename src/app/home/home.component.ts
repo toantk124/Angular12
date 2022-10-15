855f397cd2b046d03a7e55997b002d9397b2cb12
toantk124 <khanhtoantran1101@gmail.com>
2022-10-15 09:30:40 +0700

    Apdung

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,17 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
- public name ='Khánh Toàn';
- public age = 25;
-public traiCay = ['Cam','Quýt','Xoài','Nho'];
+  public name = 'Khánh Toàn';
+  public age = 25;
+  public traiCay = ['Cam', 'Quýt', 'Xoài', 'Nho'];
+  public traiCay2 = [
+    { ten: 'cam', gia: 12,haGia:false },
+    { ten: 'Nho', gia: -5,haGia:true },
+    { ten: 'Quýt', gia: 14,haGia:true },
+    { ten: 'Nho', gia: 23,haGia:false},
+
+  ];
   constructor() { }
 
- public ngOnInit(): void {
-    console.log('trai cay =',this.traiCay);
+  public ngOnInit(): void {
+    console.log('trai cay =', this.traiCay);
   }
-  public resetName() :void{
+  public resetName(): void {
     console.log('resetName');
-    this.name='';
+    this.name = '';
   }
 
 }
